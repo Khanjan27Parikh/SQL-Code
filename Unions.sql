1e3e19ffd633e693d3d18b6f277f5baeb4622172
Khanjan Parikh <developerkhanju09@gmail.com>
2025-06-17 22:24:58 +0530

    UNIONS

---
+++ Unions.sql
@@ -0,0 +1,21 @@
+--Union, Union All
+
+--SELECT *
+--FROM SQLTutorial.dbo.EmployeeDemographics
+--Full Outer Join SQLTutorial.dbo.WareHouseEmployeeDemographics
+--ON EmployeeDemographics.EmployeeID = WareHouseEmployeeDemographics.EmployeeID
+
+
+--SELECT *
+--FROM SQLTutorial.dbo.EmployeeDemographics
+--UNION ALL 
+--SELECT *
+--FROM SQLTutorial.dbo.WareHouseEmployeeDemographics
+--ORDER BY EmployeeID
+
+SELECT EmployeeID, FirstName, Age
+FROM SQLTutorial.dbo.EmployeeDemographics
+UNION
+SELECT EmployeeID, JobTitle, Salary
+FROM SQLTutorial.dbo.EmployeeSalary
+ORDER BY EmployeeID
