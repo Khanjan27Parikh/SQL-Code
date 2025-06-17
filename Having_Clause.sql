5652b45c917dc0efd479f97bb282acac490bc994
Khanjan Parikh <developerkhanju09@gmail.com>
2025-06-17 22:26:07 +0530

    HAVING Clause

---
+++ Having_Clause.sql
@@ -0,0 +1,17 @@
+
+--Having Clause 
+
+SELECT JobTitle, COUNT(JobTitle)
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING COUNT(JobTitle) > 1
+
+SELECT JobTitle, AVG(Salary) AS AvgSalary
+FROM SQLTutorial.dbo.EmployeeDemographics
+JOIN SQLTutorial.dbo.EmployeeSalary
+ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+GROUP BY JobTitle
+HAVING AVG(Salary) > 45000
+ORDER BY AvgSalary
